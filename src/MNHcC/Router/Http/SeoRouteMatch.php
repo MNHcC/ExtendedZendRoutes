2380ee159b06c77ba60dd70c831a7180f7fedddd
carschrotter <carschrotter@carschrotter-work>
2016-06-28 13:06:40 +0200

    switch from manual define autoloading to classmap

--- src/MNHcC/Router/Http/SeoRouteMatch.php
+++ src/MNHcC/Router/Http/SeoRouteMatch.php
@@ -8,7 +8,9 @@
 
 namespace MNHcC\Router\Http {
 
-    use \Zend\Mvc\Router\Http\RouteMatch;
+    use \Closure;
+    use \InvalidArgumentException;
+    
     /**
      * SeoRouteMatch
      *
@@ -17,23 +19,56 @@ namespace MNHcC\Router\Http {
      * @license default
      */
     class SeoRouteMatch extends RouteMatch {
-	
-	
-	public function __construct(array $params, $length = 0) {
-	    parent::__construct($params, $length);
-	    
-	}
-	
-	public static function factory(RouteMatch $parent) {
+        
+        /**
+         * Create a part RouteMatch with given parameters and length.
+         *
+         * @param  array   $params
+         * @param  int $length
+         */
+        public function __construct(array $params, $length = 0) {
+            parent::__construct($params, $length);
+            $this->setParam('matched_aliases', []);
+        }
 
-	    $refl = new \ReflectionClass($parent);
-	    $refl_parms = $refl->getProperty('params');
-	    $refl_parms->setAccessible(true);
-	    $params = $refl_parms->getValue($parent);
-	    $refl_length = $refl->getProperty('length');
-	    $refl_length->setAccessible(true);
-	    $length = $refl_length->getValue($parent);
-	    return new static($params, $length);
+        public function addMatchedAlias($key, array $option_name) {
+            $this->params['matched_aliases'][$key] = $option_name;
+        }
+        
+        public function setMatchedAliases($matched_aliases) {
+            $this->setParam('matched_aliases', $matched_aliases);
+        }
+        
+        public function getMatchedAliases() {
+            $this->getParam('matched_aliases', []);
+        }
+        
+        
+        /**
+         * 
+         * @param \Zend\Mvc\Router\Http\RouteMatch|\Zend\Router\Http\RouteMatch $match
+         * @return static
+         * @throws InvalidArgumentException
+         */
+	public static function factory($match) {
+            $parentc = static::parentc;
+            if(!($match instanceof $parentc)){
+                throw new InvalidArgumentException('Parent muss a Instance of '. static::parentc);
+            }
+//	    $refl = new \ReflectionClass($parent);
+//	    $refl_parms = $refl->getProperty('params');
+//	    $refl_parms->setAccessible(true);
+//	    $params = $refl_parms->getValue($parent);
+//            
+//            
+//	    $refl_length = $refl->getProperty('length');
+//	    $refl_length->setAccessible(true);
+//	    $length = $refl_length->getValue($parent);
+            
+            /* g for get */
+            $g = Closure::bind(function($roperty){ return $this->$roperty;}, $match, self::parentc);
+            return (new static( $g('params'), $g('length')))
+                ->setMatchedRouteName($g('matchedRouteName'));
 	}
     }
 
