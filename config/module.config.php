89c078c6720aaa3e588a6bc5d4b04284eb4b5c6a
carschrotter <carschrotter@carschrotter-work>
2016-08-13 13:38:21 +0200

    fixing namespace

--- config/module.config.php
+++ config/module.config.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace MNHcC\ExtendedZendRoutes;
-
-return [
-    'route_manager' => [
-        'factories' => [
-            Router\Http\SeoSegment::class => Router\RouteInvokableFactory::class,
-            Router\Http\ExistingControllerSegment::class => Router\RouteInvokableFactory::class,
-        ],
-        'abstract_factories' => [
-            -1 => Router\RouteInvokableFactory::class
-        ],
-    ]
-];
+namespace MNHcC\ExtendedZendRoutes {
+    return [
+        'route_manager' => [
+            'factories' => [
+                Router\Http\SeoSegment::class => Router\RouteInvokableFactory::class,
+                Router\Http\ExistingControllerSegment::class => Router\RouteInvokableFactory::class,
+            ],
+            'abstract_factories' => [
+                -1 => Router\RouteInvokableFactory::class
+            ],
+        ]
+    ];
+}
